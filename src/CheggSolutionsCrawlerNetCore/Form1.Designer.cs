8777519df9967629ad1cb07d80d864fb1f881a8c
Hung Cao <Cao.Hung@alaskaair.com>
2019-11-15 13:52:21 -0800

    add .net core version

---
+++ src/CheggSolutionsCrawlerNetCore/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CheggSolutionsCrawlerNetCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtResultFolderPath = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(20, 21);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(610, 31);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(648, 21);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 31);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(819, 21);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(150, 31);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Password";
+            // 
+            // txtResultFolderPath
+            // 
+            this.txtResultFolderPath.Location = new System.Drawing.Point(987, 20);
+            this.txtResultFolderPath.Name = "txtResultFolderPath";
+            this.txtResultFolderPath.Size = new System.Drawing.Size(204, 31);
+            this.txtResultFolderPath.TabIndex = 1;
+            this.txtResultFolderPath.Text = "Output folder eg C:\\";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(1210, 18);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(112, 34);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 765);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtResultFolderPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtResultFolderPath;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
+
---
+++ src/CheggSolutionsCrawlerNetCore/Form1.cs
@@ -0,0 +1,160 @@
+﻿using CefSharp;
+using CefSharp.Internals;
+using CefSharp.WinForms;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheggSolutionsCrawlerNetCore
+{
+    static class Extensions
+    {
+        public static bool HasClass(this HtmlNode node, params string[] classValueArray)
+        {
+            var classValue = node.GetAttributeValue("class", "");
+            var classValues = classValue.Split(' ');
+            return classValueArray.All(c => classValues.Contains(c));
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public async Task InitializeChromium()
+        {
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser(txtUrl.Text);
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+            await LoadPageAsync(chromeBrowser);
+            chromeBrowser.ExecuteScriptAsync($"document.getElementById('emailForSignIn').value = '{txtUsername.Text}';");
+            await LoadPageAsync(chromeBrowser);
+            chromeBrowser.ExecuteScriptAsync($"document.getElementById('passwordForSignIn').value = '{txtPassword.Text}';");
+            await LoadPageAsync(chromeBrowser);
+            chromeBrowser.ExecuteScriptAsync("document.getElementsByName('login')[0].click()");
+            await LoadPageAsync(chromeBrowser);
+            await LoopThroughEachSolution();
+        }
+
+        public Task LoadPageAsync(IWebBrowser browser)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            EventHandler<LoadingStateChangedEventArgs> handler = null;
+            handler = (sender, args) =>
+            {
+                if (!args.IsLoading)
+                {
+                    browser.LoadingStateChanged -= handler;
+                    tcs.TrySetResultAsync(true);
+                }
+            };
+
+            browser.LoadingStateChanged += handler;
+            return tcs.Task;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public async Task LoopThroughEachSolution()
+        {
+            bool result = true;
+            do
+            {
+                await Crawl();
+                result = await NextPage();
+            }
+            while (result);
+        }
+
+        public async Task<bool> NextPage()
+        {
+            var source = await chromeBrowser.GetSourceAsync();
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(source);
+            var buttonNext = htmlDoc.DocumentNode.Descendants().FirstOrDefault(_ => _.Name.Equals("button") && _.GetAttributeValue("data-hover", "").Equals("Next"));
+            if (buttonNext != null)
+            {
+                chromeBrowser.ExecuteScriptAsync("document.querySelectorAll('[data-hover=Next]')[0].click();");
+                await LoadPageAsync(chromeBrowser);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task Crawl()
+        {
+            int wait = 0;
+            do
+            {
+                var source = await chromeBrowser.GetSourceAsync();
+                var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                htmlDoc.LoadHtml(source);
+
+                var title = htmlDoc.DocumentNode.Descendants().Where(_ => _.Name.Equals("h3")).FirstOrDefault(_ => _.HasClass("title"))?.InnerText;
+
+                var node = htmlDoc.DocumentNode.Descendants().Where(_ => _.Name.Equals("ol")).FirstOrDefault(_ => _.HasClass("steps"));                
+                var chapterText = htmlDoc.DocumentNode.Descendants().Where(_ => _.Name.Equals("h2")).FirstOrDefault(_ => _.GetAttributeValue("aria-pressed", "").Equals("true"))?.InnerText;
+
+                System.IO.Directory.CreateDirectory($"{txtResultFolderPath.Text}\\{chapterText}");
+                if (title != null && node != null)
+                {
+                    WriteToFile($"{txtResultFolderPath.Text}\\{chapterText}\\{title}.html", node.OuterHtml);
+                    break;
+                }
+                else
+                {
+                    var noSolutionBtn = htmlDoc.DocumentNode.Descendants().Where(_ => _.Name.Equals("button")).FirstOrDefault(_ => _.HasClass("simple-button", "cta"));
+
+                    if (noSolutionBtn != null)
+                    {
+                        WriteToFile($"{txtResultFolderPath.Text}\\{chapterText}\\{title}-nosolution.html", "no solution");
+                        break;
+                    }
+
+                    wait++;
+                    await Task.Delay(2000);
+                }
+            }
+            while (wait <= 10);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void WriteToFile(string filePath, string content)
+        {
+            using (FileStream fs = File.Create(filePath))
+            {
+                // writing data in string
+                string dataasstring = content;
+                byte[] info = new UTF8Encoding(true).GetBytes(dataasstring);
+                fs.Write(info, 0, info.Length);
+
+                // writing data in bytes already
+                byte[] data = new byte[] { 0x0 };
+                fs.Write(data, 0, data.Length);
+            }
+        }
+
+        private async void btnExecute_Click(object sender, EventArgs e)
+        {
+            await InitializeChromium();
+        }
+    }
+}
